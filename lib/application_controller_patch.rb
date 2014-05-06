b1d74eafd989453e90f45ed94dcfef9e0da7d04b
pinzolo <pinzolo@gmail.com>
2014-05-06 11:53:25 +0900

    Refactor ApplicationControllerPath that uses ActiveSupport::Concern

--- lib/application_controller_patch.rb
+++ lib/application_controller_patch.rb
@@ -1,15 +1,21 @@
-if require "application_controller"
-  class ApplicationController < ActionController::Base
+module ApplicationControllerPatch
+  extend ActiveSupport::Concern
+
+  included do
     if respond_to?(:before_action)
       before_action :store_pwfmt_format
     else
       before_filter :store_pwfmt_format
     end
-
-    private
-
-    def store_pwfmt_format
-      PwfmtContext.format = params[:pwfmt][:format] if params[:pwfmt]
-    end
   end
+
+  private
+
+  def store_pwfmt_format
+    PwfmtContext.format = params[:pwfmt][:format] if params[:pwfmt]
+  end
+end
+
+if require "application_controller"
+  ApplicationController.send(:include, ApplicationControllerPatch)
 end
