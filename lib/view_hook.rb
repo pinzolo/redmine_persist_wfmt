625648bb1e7d032562d50657b01a6d8b73dea66e
pinzolo <pinzolo@gmail.com>
2014-05-09 01:27:16 +0900

    Apply to welcome text

--- lib/view_hook.rb
+++ lib/view_hook.rb
@@ -23,5 +23,17 @@ module Pwfmt
         javascript_tag(script)
       end
     end
+
+    def view_settings_general_form(context)
+      pwfmt = PwfmtFormat.where(field: 'settings_welcome_text').first
+      if pwfmt
+        script =<<-_EOF_
+(function() {
+  $('#pwfmt-select-settings_welcome_text').val('#{pwfmt.format}').change();
+})();
+        _EOF_
+        javascript_tag(script)
+      end
+    end
   end
 end
