5b78bc876995b0eea95073dd0a1a82854de00a7c
pinzolo <pinzolo@gmail.com>
2014-05-08 15:46:29 +0900

    Refactored

--- lib/wiki_formatting_patch.rb
+++ lib/wiki_formatting_patch.rb
@@ -1,7 +1,7 @@
 module Redmine::WikiFormatting
   class << self
     def to_html_with_pwfmt(format, text, options = {})
-      if text.try(:pwfmt)
+      if text.respond_to?(:pwfmt) && text.pwfmt && text.pwfmt.format.present?
         to_html_without_pwfmt(text.pwfmt.format, text, options)
       else
         to_html_without_pwfmt(format, text, options)
