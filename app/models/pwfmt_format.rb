2cf5f34446e022ade710cceaee7497640c3f208e
pinzolo <pinzolo@gmail.com>
2014-05-11 08:06:24 +0900

    Add persist_all task

--- app/models/pwfmt_format.rb
+++ app/models/pwfmt_format.rb
@@ -1,3 +1,12 @@
 class PwfmtFormat < ActiveRecord::Base
   unloadable
+
+  def self.persist(target, field, format)
+    pwfmt = PwfmtFormat.where(target_id: target.id, field: field).first
+    if pwfmt
+      pwfmt.update_attributes(format: format)
+    else
+      PwfmtFormat.create(target_id: target.id, field: field, format: format)
+    end
+  end
 end
