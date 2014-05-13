52dc20b4ea55ebec9566ef45b0445f95fa447691
pinzolo <pinzolo@gmail.com>
2014-05-13 09:54:41 +0900

    Set version to 0.5.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_persist_wfmt do
   name 'Redmine persist wiki format plugin'
   author 'pinzolo'
   description 'This is a plugin for Redmine that persists wiki format'
-  version '0.0.1'
+  version '0.5.0'
   url 'https://github.com/pinzolo/redimen_persist_wfmt'
   author_url 'https://github.com/pinzolo'
 end
