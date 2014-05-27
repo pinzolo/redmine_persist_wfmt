733fa59b0e481deea5d869a2904faa13d22c98ce
pinzolo <pinzolo@gmail.com>
2014-05-27 22:44:26 +0900

    Add page.driver.reset! per after spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,5 +28,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.before(:suite) { DatabaseCleaner.strategy = :truncation }
   config.before(:each)  { DatabaseCleaner.start }
-  config.after(:each)   { DatabaseCleaner.clean }
+  config.after(:each) do
+    DatabaseCleaner.clean
+    page.driver.reset!
+  end
 end
