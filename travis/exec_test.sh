0eb44babe9c2a615bf9a81688310c3f08a1443aa
pinzolo <pinzolo@gmail.com>
2014-06-03 17:12:19 +0900

    Plugin name extract to local variable in travis/exec_test.sh

--- travis/exec_test.sh
+++ travis/exec_test.sh
@@ -2,14 +2,15 @@
 
 export REDMINE_LANG=en
 export RAILS_ENV=test
+PLUGIN_NAME="redmine_persist_wfmt"
 
 # Initialize redmine
 bundle exec rake generate_secret_token
 bundle exec rake db:migrate
-#bundle exec rake redmine:load_default_data
+bundle exec rake redmine:load_default_data
 
 # Copy assets & execute plugin's migration
-bundle exec rake redmine:plugins NAME=redmine_persist_wfmt
+bundle exec rake redmine:plugins NAME=${PLUGIN_NAME}
 
 if [ $TARGET = "redmine" ]; then
   export SCMS=bazaar,cvs,subversion,git,mercurial,filesystem
@@ -21,6 +22,6 @@ else
   bundle exec rails g rspec:install
 
   # Execute plugin test by RSpec
-  bundle exec rspec plugins/redmine_persist_wfmt/spec -c
+  bundle exec rspec plugins/${PLUGIN_NAME}/spec -c
 fi
 
