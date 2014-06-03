0d65f9dae2951d8f1fd9326998a11b6922b7ec54
pinzolo <pinzolo@gmail.com>
2014-06-03 21:16:22 +0900

    Remove load_default_data from exec_test.sh

--- travis/exec_test.sh
+++ travis/exec_test.sh
@@ -7,7 +7,7 @@ PLUGIN_NAME="redmine_persist_wfmt"
 # Initialize redmine
 bundle exec rake generate_secret_token
 bundle exec rake db:migrate
-bundle exec rake redmine:load_default_data
+#bundle exec rake redmine:load_default_data
 
 # Copy assets & execute plugin's migration
 bundle exec rake redmine:plugins NAME=${PLUGIN_NAME}
