150006f48cfce6076c59ab4571c8dcf686d819b2
pinzolo <pinzolo@gmail.com>
2014-06-15 15:03:11 +0900

    Refactor news comment

--- lib/comment_patch.rb
+++ lib/comment_patch.rb
@@ -7,20 +7,11 @@ module Pwfmt::CommentPatch
 
   def load_wiki_format!
     pwfmt = PwfmtFormat.where(target_id: self.id, field: 'comment_comments').first
-    comments.pwfmt = pwfmt if comments && pwfmt
+    comments.wiki_format = pwfmt.format if comments && pwfmt
   end
 
   def persist_wiki_format
-    if Pwfmt::Context.formats && Pwfmt::Context.formats.key?('comment_comments')
-      pwfmt = PwfmtFormat.where(target_id: self.id, field: 'comment_comments').first
-      if pwfmt
-        pwfmt.update_attributes(format: Pwfmt::Context.formats['comment_comments'])
-      else
-        PwfmtFormat.create(target_id: self.id,
-                           field: 'comment_comments',
-                           format: Pwfmt::Context.formats['comment_comments'])
-      end
-    end
+    PwfmtFormat.persist(self, 'comment_comments')
   end
 end
 
