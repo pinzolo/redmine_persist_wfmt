4ba1a7b966af7810d958182e496283c7f02b33c5
pinzolo <pinzolo@gmail.com>
2014-06-15 18:07:57 +0900

    Refactor file construction

--- init.rb
+++ init.rb
@@ -1,5 +1,3 @@
-require_relative 'lib/view_hook'
-
 Redmine::Plugin.register :redmine_persist_wfmt do
   name 'Redmine persist wiki format plugin'
   author 'pinzolo'
@@ -9,33 +7,4 @@ Redmine::Plugin.register :redmine_persist_wfmt do
   author_url 'https://github.com/pinzolo'
 end
 
-require_relative 'lib/string_patch'
-require_relative 'lib/pwfmt_context'
-require_relative 'lib/pwfmt_helper'
-require_relative 'lib/textile_helper_patch'
-require_relative 'lib/markdown_helper_patch'
-require_relative 'lib/application_controller_patch'
-require_relative 'lib/wiki_formatting_patch'
-
-require_relative 'lib/project_patch'
-require_relative 'lib/projects_controller_patch'
-require_relative 'lib/issue_patch'
-require_relative 'lib/issues_controller_patch'
-require_relative 'lib/journal_patch'
-require_relative 'lib/journals_controller_patch'
-require_relative 'lib/journals_helper_patch'
-require_relative 'lib/document_patch'
-require_relative 'lib/documents_controller_patch'
-require_relative 'lib/news_patch'
-require_relative 'lib/news_controller_patch'
-require_relative 'lib/comment_patch'
-require_relative 'lib/message_patch'
-require_relative 'lib/messages_controller_patch'
-require_relative 'lib/setting_patch'
-require_relative 'lib/settings_controller_patch'
-require_relative 'lib/welcome_controller_patch'
-require_relative 'lib/wiki_content_patch'
-require_relative 'lib/wiki_content_version_patch'
-require_relative 'lib/wiki_controller_patch'
-
-require_relative 'lib/application_helper_patch'
+require_relative 'lib/pwfmt'
--- lib/pwfmt_context.rb
+++ lib/pwfmt_context.rb
@@ -1,40 +1,38 @@
-module Pwfmt
-  class Context
-    def self.formats
-      Thread.current[:pwfmt_formats]
-    end
+class Pwfmt::Context
+  def self.formats
+    Thread.current[:pwfmt_formats]
+  end
 
-    def self.formats=(formats)
-      Thread.current[:pwfmt_formats] = formats
-    end
+  def self.formats=(formats)
+    Thread.current[:pwfmt_formats] = formats
+  end
 
-    def self.reserved_format(field)
-      if Thread.current[:pwfmt_reserved_format]
-        Thread.current[:pwfmt_reserved_format][field]
-      end
+  def self.reserved_format(field)
+    if Thread.current[:pwfmt_reserved_format]
+      Thread.current[:pwfmt_reserved_format][field]
     end
+  end
 
-    def self.reserve_format(field, text)
-      if text.respond_to?(:wiki_format)
-        Thread.current[:pwfmt_reserved_format] ||= {}
-        Thread.current[:pwfmt_reserved_format][field] = text.wiki_format
-      elsif text.try(:pwfmt)
-        Thread.current[:pwfmt_reserved_format] ||= {}
-        Thread.current[:pwfmt_reserved_format][field] = text.pwfmt.format
-      end
+  def self.reserve_format(field, text)
+    if text.respond_to?(:wiki_format)
+      Thread.current[:pwfmt_reserved_format] ||= {}
+      Thread.current[:pwfmt_reserved_format][field] = text.wiki_format
+    elsif text.try(:pwfmt)
+      Thread.current[:pwfmt_reserved_format] ||= {}
+      Thread.current[:pwfmt_reserved_format][field] = text.pwfmt.format
     end
+  end
 
-    def self.clear
-      self.formats = nil
-      Thread.current[:pwfmt_reserved_format] = nil
-    end
+  def self.clear
+    self.formats = nil
+    Thread.current[:pwfmt_reserved_format] = nil
+  end
 
-    def self.has_format?(key)
-      self.formats.present? && self.formats.key?(key)
-    end
+  def self.has_format?(key)
+    self.formats.present? && self.formats.key?(key)
+  end
 
-    def self.format(key)
-      self.formats[key] if has_format?(key)
-    end
+  def self.format(key)
+    self.formats[key] if has_format?(key)
   end
 end
