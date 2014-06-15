1f6aca27a1c8f04f64296929401889be8100bd69
pinzolo <pinzolo@gmail.com>
2014-06-15 16:05:32 +0900

    Refactor welcome text

--- lib/setting_patch.rb
+++ lib/setting_patch.rb
@@ -2,28 +2,11 @@ module Pwfmt::SettingPatch
   extend ActiveSupport::Concern
 
   included do
-    after_find :load_wiki_format!
     after_save :persist_wiki_format
   end
 
-  def load_wiki_format!
-    if self.name == 'welcome_text'
-      pwfmt = PwfmtFormat.where(target_id: self.id, field: 'settings_welcome_text').first
-      value.pwfmt = pwfmt if value && pwfmt
-    end
-  end
-
   def persist_wiki_format
-    if self.name == 'welcome_text' && Pwfmt::Context.formats && Pwfmt::Context.formats.key?('settings_welcome_text')
-      pwfmt = PwfmtFormat.where(target_id: self.id, field: 'settings_welcome_text').first
-      if pwfmt
-        pwfmt.update_attributes(format: Pwfmt::Context.formats['settings_welcome_text'])
-      else
-        PwfmtFormat.create(target_id: self.id,
-                           field: 'settings_welcome_text',
-                           format: Pwfmt::Context.formats['settings_welcome_text'])
-      end
-    end
+    PwfmtFormat.persist(self, 'settings_welcome_text')
   end
 end
 
--- lib/view_hook.rb
+++ lib/view_hook.rb
@@ -1,15 +1,4 @@
 module Pwfmt
   class ViewHook < Redmine::Hook::ViewListener
-    def view_settings_general_form(context)
-      pwfmt = PwfmtFormat.where(field: 'settings_welcome_text').first
-      if pwfmt
-        script =<<-_EOF_
-(function() {
-  $('#pwfmt-select-settings_welcome_text').val('#{pwfmt.format}').change();
-})();
-        _EOF_
-        javascript_tag(script)
-      end
-    end
   end
 end
