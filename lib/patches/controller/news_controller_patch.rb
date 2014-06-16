3aca7188d91c769bf079da042b7d497b4162f7cf
pinzolo <pinzolo@gmail.com>
2014-06-16 14:16:25 +0900

    Remove AplicationHelperPatch

--- lib/patches/controller/news_controller_patch.rb
+++ lib/patches/controller/news_controller_patch.rb
@@ -3,6 +3,7 @@ module Pwfmt::NewsControllerPatch
 
   included do
     before_render :load_wiki_format, only: [:edit, :show]
+    before_render :load_all_news_wiki_format, only: [:index]
     before_render :reserve_format, only: [:edit, :show]
   end
 
@@ -13,6 +14,10 @@ module Pwfmt::NewsControllerPatch
       @news.load_wiki_format!
       @news.comments.each(&:load_wiki_format!)
     end
+  end
+
+  def load_all_news_wiki_format
+    @newss.each(&:load_wiki_format!)
   end
 
   def reserve_format
