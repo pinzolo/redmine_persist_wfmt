06fe2039b7b5cfaf0d57ddfae069dc09c1d1bb4e
pinzolo <pinzolo@gmail.com>
2019-04-30 07:21:33 +0900

    Add ui tests for document

--- test/ui/document_description_test.rb
+++ test/ui/document_description_test.rb
@@ -19,15 +19,63 @@ class DocumentDescriptionTest < Pwfmt::SystemTestCase
     assert markdown_selected?('pwfmt-select-document_description')
   end
 
-  test 'view as textile when document is saved as textile' do
+  test 'view as textile in index page when document is saved as textile' do
+    save_document_as('textile')
+    visit project_documents_path(project_id: project_id)
+    assert textile_include?('content')
+  end
+
+  test 'view as markdown in index page when document is saved as markdown' do
+    save_document_as('markdown')
+    visit project_documents_path(project_id: project_id)
+    assert markdown_include?('content')
+  end
+
+  test 'view as textile in show page when document is saved as textile' do
     save_document_as('textile')
     visit document_path(Document.first)
     assert textile_include?('content')
   end
 
-  test 'view as markdown when document is saved as markdown' do
+  test 'view as markdown in show page when document is saved as markdown' do
     save_document_as('markdown')
     visit document_path(Document.first)
     assert markdown_include?('content')
+  end
+
+  test 'textile selected when editing document saved as textile' do
+    Setting.text_formatting = 'markdown'
+    save_document_as('textile')
+    visit edit_document_path(Document.first)
+    assert textile_selected?('pwfmt-select-document_description')
+  end
+
+  test 'markdown selected when editing document saved as markdown' do
+    Setting.text_formatting = 'textile'
+    save_document_as('markdown')
+    visit edit_document_path(Document.first)
+    assert markdown_selected?('pwfmt-select-document_description')
+  end
+
+  test 'update format to markdown from textile' do
+    Setting.text_formatting = 'textile'
+    save_document_as('markdown')
+    visit edit_document_path(Document.first)
+    select_markdown('pwfmt-select-document_description')
+    find_by_id('document_description').set(markdown_text)
+    find('input[name=commit]').click
+    visit document_path(Document.first)
+    assert markdown_include?('content')
+  end
+
+  test 'update format to textile from markdown' do
+    Setting.text_formatting = 'markdown'
+    save_document_as('textile')
+    visit edit_document_path(Document.first)
+    select_textile('pwfmt-select-document_description')
+    find_by_id('document_description').set(textile_text)
+    find('input[name=commit]').click
+    visit document_path(Document.first)
+    assert textile_include?('content')
   end
 end
