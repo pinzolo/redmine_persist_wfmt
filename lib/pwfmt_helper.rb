b222b1e614d10f1afcd49b9ad9c491fd5c05519d
pinzolo <pinzolo@gmail.com>
2019-05-28 22:02:42 +0900

    Solve Layout/IndentHeredoc cop

--- lib/pwfmt_helper.rb
+++ lib/pwfmt_helper.rb
@@ -2,12 +2,12 @@
 module Pwfmt::Helper
   def pwfmt_select_script(field_id, wiki_format)
     format = Pwfmt::Context.reserved_format_for(field_id) || wiki_format
-    <<-_EOF_
-(function(doc, toolbar) {
-  pwfmt.insertFormatHidden(doc, '#{field_id}', '#{format}');
-  var formats = #{Redmine::WikiFormatting.formats_for_select};
-  pwfmt.insertFormatSelector(doc, '#{field_id}', '#{format}', formats, toolbar);
-})(window.document, wikiToolbar);
+    <<~_EOF_
+      (function(doc, toolbar) {
+        pwfmt.insertFormatHidden(doc, '#{field_id}', '#{format}');
+        var formats = #{Redmine::WikiFormatting.formats_for_select};
+        pwfmt.insertFormatSelector(doc, '#{field_id}', '#{format}', formats, toolbar);
+      })(window.document, wikiToolbar);
     _EOF_
   end
 end
