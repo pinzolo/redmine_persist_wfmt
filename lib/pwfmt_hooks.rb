17fed23c2f9918198053d0c2a9bedeaf5a25efaa
pinzolo <pinzolo@gmail.com>
2019-05-28 22:19:01 +0900

    Solve Lint/UnusedMethodArgument cop

--- lib/pwfmt_hooks.rb
+++ lib/pwfmt_hooks.rb
@@ -1,6 +1,6 @@
 # This implements redmine hook for rendring script.
 class Pwfmt::Hooks < Redmine::Hook::ViewListener
-  def view_layouts_base_html_head(context = {})
+  def view_layouts_base_html_head(_context = {})
     html = ''
     html << javascript_include_tag('pwfmt', plugin: 'redmine_persist_wfmt')
     html.html_safe
