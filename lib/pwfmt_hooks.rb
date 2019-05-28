fe1f234b69608ec757da3d052f87c3e87871ff99
pinzolo <pinzolo@gmail.com>
2019-05-28 22:23:53 +0900

    Solve Rails/OutputSafety cop

--- lib/pwfmt_hooks.rb
+++ lib/pwfmt_hooks.rb
@@ -1,8 +1,6 @@
 # This implements redmine hook for rendring script.
 class Pwfmt::Hooks < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(_context = {})
-    html = ''
-    html << javascript_include_tag('pwfmt', plugin: 'redmine_persist_wfmt')
-    html.html_safe
+    javascript_include_tag('pwfmt', plugin: 'redmine_persist_wfmt')
   end
 end
