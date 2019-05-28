ecc7879e82f28f8d4cbbbe5c9754c3d427e44ed3
pinzolo <pinzolo@gmail.com>
2019-05-28 21:58:20 +0900

    Solve Rails/ActiveRecordAliases cop

--- app/models/pwfmt_format.rb
+++ app/models/pwfmt_format.rb
@@ -6,7 +6,7 @@ class PwfmtFormat < ActiveRecord::Base
 
     pwfmt = PwfmtFormat.where(target_id: target.id, field: field).first
     if pwfmt
-      pwfmt.update_attributes(format: format)
+      pwfmt.update(format: format)
     else
       PwfmtFormat.create(target_id: target.id, field: field, format: format)
     end
