f39e32a81e25e446dbc852c4e021f023cac1df2f
pinzolo <pinzolo@gmail.com>
2019-05-28 20:08:58 +0900

    Remove debug code

--- lib/patches/helper/application_helper_patch.rb
+++ lib/patches/helper/application_helper_patch.rb
@@ -4,7 +4,6 @@ module Pwfmt::ApplicationHelperPatch
   def truncate_lines(string, options={})
     fmt = string.wiki_format
     super(string, options).tap do |s|
-      Rails.logger.debug("pwfmt: #{fmt}")
       s.wiki_format = fmt if fmt
     end
   end
